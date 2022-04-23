9d22f7d1780b5a807a8fefcfb217693519c8a06b
Maharshi Basu <basumaharshi10@gmail.com>
2022-04-23 12:48:20 +0530

    Added Record Collection Code

--- record_collection.js
+++ record_collection.js
@@ -21,7 +21,30 @@ const recordCollection = {
   
   // Only change code below this line
   function updateRecords(records, id, prop, value) {
+    // const val = prop
+
+    if (prop !== 'tracks') {
+
+      if (value.length != 0) {
+        records[id][prop] = value;
+      }
+      else {
+        delete records[id][prop];
+      }
+    }
+    else if (prop === 'tracks') {
+      if (records[id].hasOwnProperty(prop) === false) {
+        records[id][prop] = [value];
+      }
+      else if (records[id].hasOwnProperty(prop) === true && value.length != 0) {
+        records[id][prop].push(value);
+      }
+      else if (records[id].hasOwnProperty(prop) === true && value.length == 0) {
+        delete records[id][prop];
+      }
+    }
     return records;
   }
   
   updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+  console.log(recordCollection);
